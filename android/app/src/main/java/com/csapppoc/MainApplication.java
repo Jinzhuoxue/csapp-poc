606936065961b700648b36de1fe812ff366a5ab2
Jinzhuoxue <jinzhuoxue@gmail.com>
2018-06-29 16:46:14 +0800

    Add Linked resources

--- android/app/src/main/java/com/csapppoc/MainApplication.java
+++ android/app/src/main/java/com/csapppoc/MainApplication.java
@@ -3,6 +3,7 @@ package com.csapppoc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new KeychainPackage(),
             new RNI18nPackage()
       );
